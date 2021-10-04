78e16eea807d62a3c4d713a4fe72659c883aeabc
zalvador <zalvadorleo@gmail.com>
2021-10-03 22:05:52 -0500

    instalacion de webpack, babel y plugins de babel

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import createError from 'http-errors'
+import express from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import indexRouter from '@s-routes/index';
+import usersRouter from '@s-routes/users';
 
 var app = express();
 
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..','public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
