7ffad0b91f84d4d641ea765e347aabe75906e87f
zalvador <zalvadorleo@gmail.com>
2021-10-10 16:36:50 -0500

    incorporacion de estilos e instalacion de dependencias de babe-webpack, configuracion de webpack y configurar webpack en modo desarrollo

---
+++ client/index.js
@@ -0,0 +1,26 @@
+import './stylesheets/style.css'
+import './stylesheets/mystyles.css'
+
+console.log("Webpack Working!!!");
+// default parameters ES6/2015
+let show = (m = "hola") => {
+    alert(m)
+};
+
+show();
+
+function resolveAfter2Seconds(){
+    return new Promise( resolve => {
+        setTimeout(()=>{
+            resolve('resolved')
+        }, 2000);
+    });
+}
+
+async function asyncCall(){
+    console.log("Calling an async function");
+    const result = await resolveAfter2Seconds();
+    console.log(result);
+}
+
+asyncCall();
---
+++ webpack.dev.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+// 0. establecer el modo del configurador
+mode: 'development',
+// 1. especificando el archivo de entrada
+entry: './client/index.js',
+// 2. especificando la salida
+output: {
+// 3. ruta absoluta de la salida
+path: path.join(__dirname, 'public'),
+// 4 . nombre de la ruta de salida
+filename: 'js/bundle.js',
+// 5. ruta del paht publico para fines del servidor de desarrollo
+publicPath: '/'
+},
+devServer: {
+static: path.join(__dirname, 'public'),
+port: 8085,
+host: 'localhost'
+},
+module: {
+    rules: [
+        {
+            test: /\.js$/,
+            exclude: /(node_modules | bower_components)/,
+            use: [
+                {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                '@babel/preset-env',
+                                {
+                                    'modules': false,
+                                    'useBuiltIns': 'usage',
+                                    'targets': {"chrome": "80"},
+                                    'corejs': 3
+                                }
+                            ]
+                        ],
+                        "plugins": [
+                            [
+                                "module-resolver",
+                                {
+                                    "root": ["./"],
+                                    "@alias":{
+                                        "@client" : "./client",
+                                    }
+                                }
+                            ]
+                        ]
+                    }
+                }
+            ]
+        },
+        {
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader']
+        }
+    ]
+},
+plugins: [
+    new MiniCssExtractPlugin({
+        filename: 'style/app.css'
+    })
+]
+}
